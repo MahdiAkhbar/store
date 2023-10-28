1256f13b89e59f9c67624feb4dceb99be5bed624
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2023-10-28 09:20:02 +0330

    edit user route, canActivate guard to some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,17 +7,20 @@ import { WhishlistComponent } from './components/whishlist/whishlist.component';
 import { LoginComponent } from './components/login/login.component';
 import { BasketComponent } from './components/basket/basket.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { adminAuthGuard } from './shared/admin-auth.guard';
+import { adminGuard } from './shared/admin.guard';
 import { SignupComponent } from './components/signup/signup.component';
+import { authGuard } from './shared/profile-auth.guard';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
 
 const routes: Routes = [
-  { path: 'form', component: AddProductFormComponent, canActivate: [adminAuthGuard] },
   { path: '', component: ProductsComponent },
   { path: 'whishlist', component: WhishlistComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'basket', component: BasketComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+  { path: 'addproduct', component: AddProductFormComponent, canActivate: [authGuard, adminGuard] },
+  { path: 'edituser', component: EditUserComponent, canActivate: [authGuard, adminGuard]},
   { path: '**', redirectTo: '/' }
 ];
 
