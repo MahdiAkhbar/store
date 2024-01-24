3ac735759cc8599275cbe480987607d86b15b0d5
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-01-24 20:37:34 +0330

    fix bug: dropdown does not close when when click everywhere except sort button

--- src/app/shared/directives/dropdown.directive.ts
+++ src/app/shared/directives/dropdown.directive.ts
@@ -12,14 +12,28 @@ export class DropdownDirective implements AfterViewInit {
   @ContentChild('dropdownBtn', { static: true }) dropdownBtn!: ElementRef<HTMLButtonElement>;
   @ContentChild('menu', { static: true }) menu!: ElementRef<HTMLDListElement>;
 
-  @HostListener('click') changeColor() {
+  // @HostListener('click') changeColor() {
+  //   this.toggleColor = !this.toggleColor;
+  // }
+  @HostListener('click', ['$event']) onClick(event: any) {
+    event.stopImmediatePropagation();
     this.toggleColor = !this.toggleColor;
+    event.stopPropagation();
   }
-  
+
   ngAfterViewInit(): void {
     this.r2.listen(this.dropdownBtn.nativeElement, 'click', () => {
       this.dropdownBtn.nativeElement.style.color = this.toggleColor ? 'rgb(155, 155, 155)' : 'black';
       this.menu.nativeElement.classList.toggle('show');
+    });
+    this.r2.listen(this.menu.nativeElement, 'click', () => {
+      this.menu.nativeElement.classList.remove('show');
+      this.dropdownBtn.nativeElement.style.color = 'rgb(155, 155, 155)';
+    })
+    this.r2.listen('window', 'click', () => {
+      this.menu.nativeElement.classList.remove('show');
+      this.dropdownBtn.nativeElement.style.color = 'rgb(155, 155, 155)';
+      this.toggleColor = false;
     })
   }
 }
